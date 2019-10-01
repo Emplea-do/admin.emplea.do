884a0eb72f7c21c1296d8970f965d2e86c2f1ebf
Giancarlos <gc.c@outlook.com>
2019-09-30 23:25:48 -0400

    improves: update test for a simple snapshot test

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,14 +1,7 @@
-import React from 'react'
-import { fireEvent } from '@testing-library/react'
-
-import { renderWithApp } from '../../utils/tests'
-
 import App from './App'
 
 describe('containers/App', () => {
   test('works correctly', () => {
-    const { getByText } = renderWithApp(<App />)
-    const sampleTest = getByText(/test/gi)
-    expect(sampleTest).toHaveTextContent('test')
+    expect(App).toMatchSnapshot()
   })
 })
