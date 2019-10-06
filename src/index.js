ac99a2e44cedda3f94f689f0541daf95a8991e3a
Giancarlos <gc.c@outlook.com>
2019-10-06 14:30:55 -0400

    refactor: improves index layout structure (since we don't gonna use styled components

--- src/index.js
+++ src/index.js
@@ -1,23 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import { ThemeProvider } from 'emotion-theming'
-import { Global } from '@emotion/core'
-
 import App from './containers/App'
 import * as serviceWorker from './serviceWorker'
 
-import { globalCSS, theme } from './styles/'
+import './scss/main.scss'
 
-ReactDOM.render(
-  <>
-    <Global styles={globalCSS} />
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
