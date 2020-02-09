82f23f3d9f65fe76043e7b18b6e832873f83845d
Giancarlos Castillo <gc.c@outlook.com>
2020-02-09 17:49:11 -0400

    feat(Admin): adds json mock data

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import { Admin } from 'react-admin'
+import { Admin, Resource, ListGuesser } from 'react-admin'
 import jsonServerProvider from 'ra-data-json-server'
 
-const dataProvider = jsonServerProvider('localhost')
+const dataProvider = jsonServerProvider('http://localhost:3000')
 
 function App() {
-  return <Admin dataProvider={dataProvider} />
+  return (
+    <Admin dataProvider={dataProvider}>
+      <Resource name="jobs" list={ListGuesser} />
+    </Admin>
+  )
 }
 
 export default App
