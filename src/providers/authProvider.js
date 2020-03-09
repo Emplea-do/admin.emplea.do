902c08624bb9fcf4a983fb134c017fb1dfcfcf13
Amaury Gomez <amaurygomez1993@gmail.com>
2020-03-09 19:20:06 -0400

    Revert "WIP: AuthProvider Authentication Strategy"

--- src/providers/authProvider.js
+++ src/providers/authProvider.js
@@ -1,59 +1,32 @@
-import { propOr, ifElse, equals, compose } from 'ramda'
-
-const requiredParameter = k => {
-  throw new Error(`${k} is required`)
-}
-const setOnLS = (
-  k = requiredParameter('key parameter'),
-  v = requiredParameter('value parameter')
-) => localStorage.setItem(k, v)
-
 /**
- * TODO:
- * - Send Google Token to API for receiven an API A.T
+ * TODO: Adds Google Auth Strategy
  */
+
 const AuthProvider = {
-  login: response => {
-    const getToken = propOr('', 'accessToken')
-    const getProfile = propOr({}, 'profileObj')
-
-    const token = getToken(response)
-    const profile = getProfile(response)
-
-    const hasProps = Boolean(token && profile)
-
-    const setDetails = () => {
-      setOnLS('token', getToken(response))
-      setOnLS('profile', JSON.stringify(getProfile(response)))
-    }
-
-    const onFailure = () => {
-      throw new Error('Ups, there was an error with the Authentication')
-    }
-
-    const onValid = compose(setDetails)
-    const canNavigate = ifElse(equals(true), onValid, onFailure)
-    canNavigate(hasProps)
-
+  login: ({ username }) => {
+    localStorage.setItem('username', username)
+    // Fake Succesfully auth
     return Promise.resolve()
   },
 
   // called when the user clicks on the logout button
   logout: () => {
-    localStorage.clear()
+    localStorage.removeItem('username')
     return Promise.resolve()
   },
   // called when the API returns an error
   checkError: ({ status }) => {
     if (status === 401 || status === 403) {
-      localStorage.clear()
+      localStorage.removeItem('username')
       return Promise.reject()
     }
     return Promise.resolve()
   },
   // called when the user navigates to a new location, to check for authentication
   checkAuth: () => {
-    return localStorage.getItem('token') ? Promise.resolve() : Promise.reject()
+    return localStorage.getItem('username')
+      ? Promise.resolve()
+      : Promise.reject()
   },
   // called when the user navigates to a new location, to check for permissions / roles
   getPermissions: () => Promise.resolve(),
